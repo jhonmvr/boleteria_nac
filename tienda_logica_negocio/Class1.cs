0dc1dbf396cc5c5d9743dccb00084a7c46dbb6e5
luis.tamayo <luis.tamayo>
2024-01-06 17:27:35 -0500

    primer commit

---
+++ tienda_logica_negocio/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tienda_logica_negocio
+{
+    public class Class1
+    {
+    }
+}
